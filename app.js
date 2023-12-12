ffe15dd22c5cbd463dd1acba738eb95f5cc07a1a
Ezezamaka Emeka Favour (FAVE) <153594689+Cryptexwin@users.noreply.github.com>
2023-12-12 11:53:16 +0100

    stat 3

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 
 const app = express()
 
-app.use(express.static(public))
+app.use(express.static('public'))
 
 app.get('/', function(req, res){
     const htmlFilePath = path.join(__dirname, 'views', 'index.html')
